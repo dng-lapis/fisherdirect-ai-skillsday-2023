1e31c0d48c2b20663202d1d9100b59b1bb6d7627
Danh Nguyen <dng@Danhs-MacBook-Air.local>
2023-10-09 16:49:45 +1100

    feat(all): Prep for deploy

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,14 +1,25 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
     "fisheraiskillsday23": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
-      "IdentityPoolName": "string"
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
   },
   "interactions": {
     "chatbot": {
       "BotName": "string",
       "FunctionArn": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "s3cac3f915": {
+      "BucketName": "string",
       "Region": "string"
     }
   }
