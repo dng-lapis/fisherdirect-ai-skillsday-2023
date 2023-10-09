c1bf939ef90d20d83860f71168d05b0feda6be66
Danh Nguyen <dng@Danhs-MacBook-Air.local>
2023-10-09 19:06:39 +1100

    feat(all): Initial dev chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,93 @@
-import React from "react";
-import logo from "./logo.svg";
+import { Interactions } from "aws-amplify";
 import "./App.css";
+import { Send } from "react-feather";
+import { useState } from "react";
 
 function App() {
+  const [messageHistory, setMessageHistory] = useState<string[]>([]);
+  const [userInput, setUserInput] = useState(null);
+  // Provide a bot name and user input
+  const sendQuestion = async () => {
+    const response = await Interactions.send(
+      "OrderFlowers_dev",
+      userInput ?? ""
+    );
+    setMessageHistory((prev) => [...prev, userInput ?? "", response.message]);
+  };
+
+  const onChangeHandler = (event: any) => {
+    setUserInput(event.target.value);
+  };
+
+  // Log chatbot response
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">Say Hello to the World's Best FisherDirect Assistant - Powered by AI</h1>
-      </header>
+      <h1 className="text-3xl font-bold underline m-8 p-4">
+        Say Hello to the World's Best FisherDirect Assistant - Powered by AI
+      </h1>
+      <CenteredOneColumn className="mt-20">
+        <CenteredOneColumn width="30rem" className="mb-20">
+          {messageHistory.length !== 0 && (
+            <ChatBlob messageHistory={messageHistory} />
+          )}
+        </CenteredOneColumn>
+        <div className="flex justify-center">
+          <input
+            type="text"
+            placeholder="Ask me anything..."
+            className="input input-bordered w-full max-w-xs"
+            onChange={onChangeHandler}
+            value={userInput ?? ""}
+          />
+          <button className="btn btn-secondary" onClick={sendQuestion}>
+            <Send />
+          </button>
+        </div>
+      </CenteredOneColumn>
     </div>
   );
 }
+
+interface CenteredOneColumnProps {
+  children: React.ReactNode;
+  width?: string;
+  className?: string;
+}
+
+export const CenteredOneColumn = ({
+  children,
+  width = "62.5rem",
+  className = "",
+}: CenteredOneColumnProps) => {
+  return (
+    <div
+      style={{ maxWidth: width }}
+      className={`container px-4 lg:px-0 mx-auto ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+const ChatBlob = ({ messageHistory }: { messageHistory: string[] }) => (
+  <>
+    {messageHistory.map((message, index) => {
+      if (index % 2 === 0) {
+        return (
+          <div className="chat chat-end" key={index}>
+            <div className="chat-bubble">{message}</div>
+          </div>
+        );
+      } else {
+        return (
+          <div className="chat chat-start" key={index}>
+            <div className="chat-bubble">{message}</div>
+          </div>
+        );
+      }
+    })}
+  </>
+);
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  daisyui: {
+    themes: [
+      {
+        cyberpunk: {
+          ...require("daisyui/src/theming/themes")["[data-theme=cyberpunk]"],
+          // primary: "#e8b166",
+          // secondary: "#c7fc9c",
+          // accent: "#d36243",
+          // neutral: "#241929",
+          // "base-100": "#e6e7ea",
+          // info: "#59c5e8",
+          // success: "#18a075",
+          // warning: "#efd348",
+          // error: "#e8352c",
+        },
+      },
+      {
+        cupcake: {
+          ...require("daisyui/src/theming/themes")["[data-theme=cupcake]"],
+        }
+      }
+    ],
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+};
