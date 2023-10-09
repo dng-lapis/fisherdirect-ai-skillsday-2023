27ca373460fd22f5e724e137965bb99ca5e4ecb8
Danh Nguyen <dng@Danhs-MacBook-Air.local>
2023-10-09 16:30:17 +1100

    feat(all): Initial commit - create-react-app

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "fisheraiskillsday23": {
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string"
+    }
+  },
+  "interactions": {
+    "chatbot": {
+      "BotName": "string",
+      "FunctionArn": "string",
+      "Region": "string"
+    }
+  }
+}
