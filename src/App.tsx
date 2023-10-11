dea196446de4273470f746899e8e6f56c1ce5ca9
Danh Nguyen <Danh.nguyen@lapisit.com.au>
2023-10-11 15:22:23 +1100

    feat(all): Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,154 @@
 import { Interactions } from "aws-amplify";
 import "./App.css";
 import { Send } from "react-feather";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+interface ContainerProps {
+  children: React.ReactNode;
+  width?: string;
+  className?: string;
+}
 
 function App() {
-  const [messageHistory, setMessageHistory] = useState<string[]>([]);
+  const [messageHistory, setMessageHistory] = useState<string[]>([
+    "Hi! 👋 I'm the FisherDirect Onboarding Assistant. What would you like to learn more about?",
+  ]);
+  const [loading, setLoading] = useState(false);
+  const scrollEnd = useRef<HTMLDivElement>(null);
   const [userInput, setUserInput] = useState<string>("");
   // Provide a bot name and user input
   const sendQuestion = async () => {
     if (userInput === "") {
       return;
     }
-    const response = await Interactions.send("OrderFlowers_main", userInput);
-    setMessageHistory((prev) => [...prev, userInput, response.message]);
+    setMessageHistory((prev) => [...prev, userInput]);
+    setLoading(true);
+    setTimeout(async () => {
+      let response: any = null;
+      try {
+        response = await Interactions.send("OrderFlowers_dev", userInput);
+        setMessageHistory((prev) => [...prev, response?.message]);
+        console.log(response);
+      } catch (e) {
+        setMessageHistory((prev) => [...prev, "Sorry, something went wrong!"]);
+      }
+      setLoading(false);
+    }, 2000);
+
     setUserInput("");
   };
+
+  useEffect(() => {
+    if (messageHistory.length > 2) {
+      scrollEnd?.current?.scrollIntoView({ behavior: "smooth", block: "end" });
+    }
+  }, [messageHistory.length]);
 
   const onChangeHandler = (event: any) => {
     setUserInput(event.target.value);
   };
 
-  // Log chatbot response
+  const onKeydown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "Enter") {
+      sendQuestion();
+    }
+  };
 
   return (
-    <div className="App">
-      <div className="writer">
-        <h1 className="text-3xl font-bold underline m-8 p-4 writer-text">
-          Hi! 👋 I'm the FisherDirect Assistant
-        </h1>
-      </div>
-      <CenteredOneColumn className="mt-20">
-        <CenteredOneColumn width="30rem" className="mb-20">
-          {messageHistory.length !== 0 && (
-            <ChatBlob messageHistory={messageHistory} />
-          )}
-        </CenteredOneColumn>
-        <div className="flex justify-center">
-          <input
-            type="text"
-            placeholder="Ask me anything..."
-            className="input input-bordered w-full max-w-xs"
-            onChange={onChangeHandler}
-            value={userInput}
-          />
-          <div className="ml-2">
-            <button className="btn" onClick={sendQuestion}>
-              <Send />
-            </button>
+    <>
+      <Header />
+      <div className="App">
+        <Container className="mt-20" width="450px">
+          <div className="border-2 rounded-3xl h-[750px] bg-white border-grey-200 shadow-2xl">
+            <div className="flex flex-col justify-between content-between h-full">
+              <div className="p-5 mt-1">
+                {messageHistory.length !== 0 && (
+                  <div className="max-h-[600px] overflow-auto">
+                    <ChatWindow
+                      messageHistory={messageHistory}
+                      loading={loading}
+                    />
+                    <div ref={scrollEnd} />
+                  </div>
+                )}
+              </div>
+              <div>
+                <hr className="border" />
+                <div className="flex justify-center p-5 mt-1">
+                  <input
+                    type="text"
+                    placeholder="Ask me anything..."
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={onChangeHandler}
+                    value={userInput}
+                    onKeyDown={onKeydown}
+                  />
+                  <div className="ml-2">
+                    <button
+                      className="btn btn-secondary"
+                      onClick={sendQuestion}
+                    >
+                      <Send />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </CenteredOneColumn>
-    </div>
+        </Container>
+      </div>
+      <Footer />
+    </>
   );
 }
 
-interface CenteredOneColumnProps {
-  children: React.ReactNode;
-  width?: string;
-  className?: string;
-}
+const Header = () => {
+  return (
+    <header className="fixed top-0 bg-primary w-full">
+      <div className="md:flex md:items-center md:justify-between mx-5 shadow-2xl">
+        <div className="min-w-0 flex-1 ">
+          <div className="py-4 ">
+            <img src="/NSWLogo.png" alt="logo" width="200px" />
+          </div>
+        </div>
+        <div className="mt-4 flex md:ml-4 md:mt-0 font-semibold text-white text-sm items-center">
+          <span>Welcome, Danh.</span>
+          <span className="ml-3 font-semibold text-xs underline">Logout</span>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export const CenteredOneColumn = ({
+const Footer = () => {
+  return (
+    <header className="fixed bottom-0 bg-white w-full">
+      <div className="md:flex md:items-center md:justify-between mx-5 shadow-2xl">
+        <div className="min-w-0 flex-1 ">
+          <div className="py-3 text-xs font-semibold flex items-center">
+            <span className="mr-1 w-8">
+              {" "}
+              <img src="/lex-logo.png" alt="lex-logo" />
+            </span>
+            Powered by Amazon Lex AI{" "}
+          </div>
+        </div>
+        <div className="mt-3 flex md:ml-4 md:mt-0 text-xs font-semibold">
+          Lapis Skills Day 2023
+        </div>
+      </div>
+    </header>
+  );
+};
+
+const Container = ({
   children,
   width = "62.5rem",
   className = "",
-}: CenteredOneColumnProps) => {
+}: ContainerProps) => {
   return (
     <div
-      style={{ maxWidth: width }}
+      style={{ width }}
       className={`container px-4 lg:px-0 mx-auto ${className}`}
     >
       {children}
@@ -75,23 +156,50 @@ export const CenteredOneColumn = ({
   );
 };
 
-const ChatBlob = ({ messageHistory }: { messageHistory: string[] }) => (
+const ChatWindow = ({
+  messageHistory,
+  loading,
+}: {
+  messageHistory: string[];
+  loading: boolean;
+}) => (
   <>
     {messageHistory.map((message, index) => {
-      if (index % 2 === 0) {
+      if (index % 2 !== 0) {
+        // This is the user
         return (
           <div className="chat chat-end" key={index}>
-            <div className="chat-bubble chat-bubble-primary">{message}</div>
+            <div className="chat-bubble chat-bubble-accent text-white text-end max-w-[250px]">
+              {message}
+            </div>
           </div>
         );
       } else {
+        // This is the bot
         return (
           <div className="chat chat-start" key={index}>
-            <div className="chat-bubble chat-bubble-secondary">{message}</div>
+            <div className="chat-image avatar online">
+              <div className="w-8 rounded-full">
+                <img src="/FisherMobile-logo.png" alt="chatbot-avatar" />
+              </div>
+            </div>
+            <div className="chat-bubble chat-bubble-primary text-start max-w-[250px]">{message}</div>
           </div>
         );
       }
     })}
+    {loading && (
+      <div className="chat chat-start" key="loading">
+        <div className="chat-image avatar online">
+          <div className="w-8 rounded-full">
+            <img src="/FisherMobile-logo.png" alt="chatbot-avatar" />
+          </div>
+        </div>
+        <div className="chat-bubble chat-bubble-primary">
+          <span className="loading loading-dots loading-md"></span>
+        </div>
+      </div>
+    )}
   </>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,22 +9,31 @@ module.exports = {
       // {
       //   cyberpunk: {
       //     ...require("daisyui/src/theming/themes")["[data-theme=cyberpunk]"],
-          // primary: "#e8b166",
-          // secondary: "#c7fc9c",
-          // accent: "#d36243",
+      // primary: "#e8b166",
+      // secondary: "#c7fc9c",
+      // accent: "#d36243",
+      // neutral: "#241929",
+      // "base-100": "#e6e7ea",
+      // info: "#59c5e8",
+      // success: "#18a075",
+      // warning: "#efd348",
+      // error: "#e8352c",
+      // },
+      // },
+      {
+        cupcake: {
+          ...require("daisyui/src/theming/themes")["[data-theme=cupcake]"],
+          primary: "#063F5C",
+          secondary: "#53758a",
+          accent: "#f58015",
           // neutral: "#241929",
           // "base-100": "#e6e7ea",
           // info: "#59c5e8",
           // success: "#18a075",
           // warning: "#efd348",
           // error: "#e8352c",
-        // },
-      // },
-      {
-        cupcake: {
-          ...require("daisyui/src/theming/themes")["[data-theme=cupcake]"],
-        }
-      }
+        },
+      },
     ],
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
